1d67e19cd28888581bbd9f232f426efd61292d41
Alan <alan-vallve@hexad.de>
2022-05-29 21:25:56 +0200

    codigo lectura muestra

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        /**
+         * Ejemplo para leer valores escritos por terminal
+         */
+        try {
+
+            //Iniciamos la clase que accedera a la terminal
+            BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+
+            /**
+             * Mostramos un mensaje por terminal para hacer saber al usuario que tiene que introducir un valor
+             */
+            System.out.println("Introduce tu dia de nacimiento");
+            //buffer.readLine(); recoge el valor escrito por el usuario
+            String diaSr = buffer.readLine();
+
+            System.out.println(diaSr);
+
+            /**
+             * Pasar String a int
+             */
+            int dia = Integer.parseInt(diaSr);
+
+            System.out.println(dia + 1);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
